a22a6f5739b288f3205b37418cb681986edaecfd
shuonanchen <shuonan.ch@gmail.cpom>
2026-02-12 13:40:54 -0800

    update hyperparameters default

--- code/ccf_imputations.py
+++ code/ccf_imputations.py
@@ -2,12 +2,13 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import processing, imputations
 
-def impute_spatial_coordinates(adata_query, adata_ref, k=3, epsilon=1e-10, similarity_transform="inverse", tau=1.0):
+def impute_spatial_coordinates(adata_query, adata_ref, k=3, epsilon=1e-10, similarity_transform="inverse", tau=0.1, sigma=0.1):
     """
     Impute spatial coordinates for query cells based on reference cells using k-nearest neighbors.
     epsilon     : float -> Small constant
     similarity_transform : {"inverse", "softmax",'gaussian'} -> How to convert distances into weights
     tau         : float -> Temperature for softmax
+    sigma       : float -> Bandwidth for gaussian kernel
     """
     common_genes = np.intersect1d(adata_ref.var_names, adata_query.var_names)
     X_query = adata_query[:, common_genes].X
@@ -19,11 +20,11 @@ def impute_spatial_coordinates(adata_query, adata_ref, k=3, epsilon=1e-10, simil
     distances, indices = nbrs.kneighbors(X_query_norm)
 
     if similarity_transform=='softmax':
-        weights = imputations.weight_softmax_dst(distances, tau = 0.1)
+        weights = imputations.weight_softmax_dst(distances, tau=tau)
     elif similarity_transform=='inverse':
-        weights = imputations.weight_inverse_dst(distances,epsilon = 1e-10)
+        weights = imputations.weight_inverse_dst(distances, epsilon=epsilon)
     elif similarity_transform=='gaussian':
-        weights = imputations.weight_gaussian_dst(distances)
+        weights = imputations.weight_gaussian_dst(distances, sigma=sigma)
     else:
         raise ValueError("similarity_transform must be 'inverse' or 'softmax' or 'gaussian'")
 
